197c44a2e38f5f1b7d17765a4f28ba0ea74a4de2
Maria Arlyn Fuerte <59829760+Mariaarlyn14@users.noreply.github.com>
2020-10-25 14:31:02 +0800

    Update Fuerte.Activity1.java

--- Fuerte/Fuerte.Activity1.java
+++ Fuerte/Fuerte.Activity1.java
@@ -11,11 +11,8 @@ package activity1;
  */
 public class Activity1 {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args)
+    {
         int number = 10;
         char letter = 'a';
         boolean result = true;
