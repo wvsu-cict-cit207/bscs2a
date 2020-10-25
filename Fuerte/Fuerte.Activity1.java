3cd607a55941a475d5236aa6c12c4713ef637c38
Maria Arlyn Fuerte <59829760+Mariaarlyn14@users.noreply.github.com>
2020-10-25 14:28:34 +0800

    Add files via upload
    Create Pull Request

---
+++ Fuerte/Fuerte.Activity1.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1;
+
+/**
+ *
+ * @author Maria Arlyn
+ */
+public class Activity1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int number = 10;
+        char letter = 'a';
+        boolean result = true;
+        
+        String str = "hello" ;
+        System.out.println("number = " + number);
+        System.out.println("letter = " + letter);
+        System.out.println("result = " + result);
+        System.out.println("str = " + str);
+    }
+    
+}
