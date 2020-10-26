1d7c356dcc68f4d61f89d383f8ed9dcab6415b1a
Jessa Mae F. Nopueto <59869722+joxygn@users.noreply.github.com>
2020-10-26 09:22:03 +0800

    Activity 1-3 Nopueto

---
+++ Nopueto/NopuetoActivity1/Exercise2.java
@@ -0,0 +1,20 @@
+
+package Activity1;
+
+public class Exercise2 {
+    public static void main (String[]args) {
+        int number1, number2, number3, average;
+        
+        number1 = 10;
+        number2 = 20;
+        number3 = 45;
+        
+        System.out.print(" number1= "+ number1);
+        System.out.print(" number2= "+ number2);
+        System.out.print(" number3= "+ number3);
+        
+        average=(number1 + number2 + number3)/3;
+        System.out.print(" Average is=" + average); 
+        
+    }
+}
