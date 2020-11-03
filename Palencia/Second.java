767ac5339e355457b9c6bcfa14e4c8e969724a4d
mybellen <72476221+mybellen@users.noreply.github.com>
2020-11-03 08:58:10 +0800

    Add files via upload
    Activity- 5

---
+++ Palencia/Second.java
@@ -0,0 +1,11 @@
+public class Second {
+
+	public static void main(String[] args) {
+		for(int i = 1; i < 6; i++) {
+			for(int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
