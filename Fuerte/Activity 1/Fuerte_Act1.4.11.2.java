1dfd66ff21eab82729131190f7d73b310b12afdf
Maria Arlyn Fuerte <59829760+Mariaarlyn14@users.noreply.github.com>
2020-11-03 10:28:30 +0800

    Add files via upload

---
+++ Fuerte/Activity 1/Fuerte_Act1.4.11.2.java
@@ -0,0 +1,12 @@
+public class Fuerte_Act1 {
+    
+    public static void main(String[] args) {
+        int a = 10, b = 20, c = 45;
+        int average = (a+b+c)/3;
+        System.out.println("number 1 = " + a);
+        System.out.println("number 2 = " + b);
+        System.out.println("number 3 = " + c);
+        System.out.println("Average is = " + average);
+    }
+    
+}
