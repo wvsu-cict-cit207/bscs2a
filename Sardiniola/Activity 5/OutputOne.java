02cdef7e919cb4a33f45ebe5a0d9658e3376c130
Aljhsrdnl <blackgreen0721@gmail.com>
2020-11-03 11:33:29 +0800

    Activity 5

---
+++ Sardiniola/Activity 5/OutputOne.java
@@ -0,0 +1,10 @@
+public class OutputOne{
+    public static void main (String[] args) {
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 8; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
