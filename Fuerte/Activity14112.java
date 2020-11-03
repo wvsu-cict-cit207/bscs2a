732942a8e153dff31cc61008a8b3077461487a42
Maria Arlyn Fuerte <59829760+Mariaarlyn14@users.noreply.github.com>
2020-11-03 09:48:11 +0800

    Add files via upload

---
+++ Fuerte/Activity14112.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package activity1.pkg4.pkg11.pkg2;
+
+/**
+ *
+ * @author Maria Arlyn
+ */
+public class Activity14112 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a = 10, b = 20, c = 45;
+        int average = (a+b+c)/3;
+        System.out.println("number 1 = " + a);
+        System.out.println("number 2 = " + b);
+        System.out.println("number 3 = " + c);
+        System.out.println("Average is = " + average);
+    }
+    
+}
