fe11587202a68bef0b3c0e10a9a2d283e12fc050
Maria Arlyn Fuerte <59829760+Mariaarlyn14@users.noreply.github.com>
2020-11-08 14:35:45 +0800

    Add files via upload

---
+++ Fuerte/Activity 5/Fuerte_Act 5.2.java
@@ -0,0 +1,14 @@
+//@author Maria Arlyn R. Fuerte (BSCS 2A)
+//Activity 5.2
+public class Fuerte_Act5 {
+
+    public static void main(String[] args) {
+        for(int i=0; i<6; i++){
+            for(int j=0; j<i; j++){
+                System.out.print("*");
+            }
+            System.out.print("\n");
+        }
+    }
+    
+}
