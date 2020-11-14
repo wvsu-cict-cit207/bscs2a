874f29c89d51b77e20a72325ded363c9675c2785
mybellen <72476221+mybellen@users.noreply.github.com>
2020-11-14 19:57:26 +0800

    Add files via upload
    Activity-1

---
+++ Palencia/Variables.java
@@ -0,0 +1,18 @@
+package activity1;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		int number = 10;
+		char letter = 'a';
+		boolean result= true;
+		String str = "hello";
+		
+		System.out.println("Number = "+ number);
+		System.out.println("Letter = "+ letter);
+		System.out.println("Result = "+ result);
+		System.out.println("str = " + str);
+
+	}
+
+}
