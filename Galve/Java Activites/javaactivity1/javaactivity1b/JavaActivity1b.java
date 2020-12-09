e7781f3c40aa936cd13cd27401afb93dd7bd9b77
Theo021 <59826172+Theo021@users.noreply.github.com>
2020-12-10 01:11:17 +0800

    Update JavaActivity1b.java

--- Galve/Java Activites/javaactivity1/javaactivity1b/JavaActivity1b.java
+++ Galve/Java Activites/javaactivity1/javaactivity1b/JavaActivity1b.java
@@ -1,22 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javaactivity1b;
 
-/**
- *
- * @author ACER
- */
 public class JavaActivity1b {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        
+       
         int num1, num2, num3, Average;
         
        num1 = 10;
