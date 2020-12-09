49722aa6951deee45cb73e888c47501a024c0fb4
Theo021 <59826172+Theo021@users.noreply.github.com>
2020-12-10 01:37:05 +0800

    Update JavaActivity1c.java

--- Galve/Java Activites/javaactivity1/javaactivity1c/JavaActivity1c.java
+++ Galve/Java Activites/javaactivity1/javaactivity1c/JavaActivity1c.java
@@ -1,9 +1,6 @@
 package javaactivity1c;
-
-public class JavaActivity1c {
-    
+public class JavaActivity1c { 
     public static void main(String[] args) {
-        // TODO code application logic here
         
         int num1, num2, num3, highestNum;
         
@@ -11,7 +8,6 @@ public class JavaActivity1c {
         num2 = 23;
         num3 = 5;
         
-
         highestNum = num3(num1>num2 ? num1:num2)? num3:((num1>num2)?num1:num2);
         System.out.println("number 1: "+num1);
         System.out.println("number 2: "+num2);
@@ -19,7 +15,6 @@ public class JavaActivity1c {
         System.out.println("The highest number is "+highestNum);
         
     }
-
     public static boolean num3(int i) {
        return false;
     }
